3e144f148a77d7e7fee14f36fd3252cc1a5272d4
Bruno Fortunato <bruno.fortunato@applicamobile.com>
2014-10-29 18:20:43 +0100

    Improved options manager with possibility to specify variables in variables

--- framework-library/src/main/java/applica/framework/library/options/PropertiesOptionManager.java
+++ framework-library/src/main/java/applica/framework/library/options/PropertiesOptionManager.java
@@ -1,5 +1,6 @@
 package applica.framework.library.options;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -9,6 +10,8 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PropertiesOptionManager implements OptionsManager {
 
@@ -40,10 +43,31 @@ public class PropertiesOptionManager implements OptionsManager {
         inputStream.close();
     }
 
+    public void load(Properties properties) {
+        this.properties = properties;
+    }
+
     @Override
     public String get(String key) {
         String environment = properties.getProperty("environment", "");
-        return properties.getProperty(String.format("%s.%s", environment, key), properties.getProperty(key));
+        String value = properties.getProperty(String.format("%s.%s", environment, key), properties.getProperty(key));
+        if (StringUtils.isNotEmpty(value)) {
+            Pattern pattern = Pattern.compile("\\$\\{\\w+\\}*", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(value);
+            if (matcher.matches()) {
+                while (matcher.find()) {
+                    String var = matcher.group();
+                    String varOption = var.substring(2, var.length() - 1);
+                    String childValue = get(varOption);
+
+                    if (StringUtils.isNotEmpty(childValue)) {
+                        value = value.replaceAll(String.format("\\$\\{%s\\}", varOption), childValue);
+                    }
+                }
+            }
+        }
+
+        return value;
     }
 
     public String getPath() {
---
+++ framework-library/src/test/java/applica/framework/library/tests/OptionsTest.java
@@ -0,0 +1,40 @@
+package applica.framework.library.tests;
+
+import applica.framework.library.options.PropertiesOptionManager;
+import junit.framework.Assert;
+
+import java.util.Properties;
+
+/**
+ * Applica (www.applicamobile.com)
+ * User: bimbobruno
+ * Date: 29/10/14
+ * Time: 17:57
+ */
+public class OptionsTest {
+
+    @org.junit.Test
+    public void options() {
+        PropertiesOptionManager options = new PropertiesOptionManager();
+        Properties properties = new Properties();
+
+        properties.put("environment", "test");
+        properties.put("name", "bruno");
+        properties.put("surname", "fortunato");
+        properties.put("test.surname", "tested");
+        properties.put("fullName", "${name} ${surname}");
+
+        options.load(properties);
+
+        Assert.assertEquals("bruno", options.get("name"));
+        Assert.assertEquals("tested", options.get("surname"));
+        Assert.assertEquals("bruno tested", options.get("fullName"));
+
+        properties.setProperty("environment", "");
+
+        Assert.assertEquals("bruno", options.get("name"));
+        Assert.assertEquals("fortunato", options.get("surname"));
+        Assert.assertEquals("bruno fortunato", options.get("fullName"));
+    }
+
+}
