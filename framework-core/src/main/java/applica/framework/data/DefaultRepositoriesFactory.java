e5fcbf062e271f19fc5ce690710e7a7f2b9c59e8
Paolo Inaudi <p91paul@gmail.com>
2014-11-02 16:00:22 +0100

    Null robust equals usage

--- framework-core/src/main/java/applica/framework/data/DefaultRepositoriesFactory.java
+++ framework-core/src/main/java/applica/framework/data/DefaultRepositoriesFactory.java
@@ -1,22 +1,12 @@
 package applica.framework.data;
 
-import applica.framework.ApplicationContextProvider;
-import applica.framework.CrudConfiguration;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 /**
- * Applica (www.applicadoit.com)
- * User: bimbobruno
- * Date: 08/10/14
- * Time: 15:24
+ * Applica (www.applicadoit.com) User: bimbobruno Date: 08/10/14 Time: 15:24
  */
-
 /**
  * This is the default implementation for repositories factory
  */
@@ -30,12 +20,15 @@ public class DefaultRepositoriesFactory implements RepositoriesFactory {
         Repository repository = null;
 
         try {
-            repository = applicationContext.getBeansOfType(Repository.class).values().stream()
-                    .filter(r -> r.getEntityType().equals(type))
+            repository = applicationContext.getBeansOfType(Repository.class).
+                    values().stream()
+                    .filter(r -> type.equals(r.getEntityType()))
                     .findFirst()
-                    .orElseThrow(() -> new NoSuchBeanDefinitionException("repository " + type.getName()));
+                    .orElseThrow(() -> new NoSuchBeanDefinitionException(
+                                    "repository " + type.getName()));
         } catch (NoSuchBeanDefinitionException e) {
-            repository = (DefaultRepository) applicationContext.getBean("default-repository");
+            repository = (DefaultRepository) applicationContext.getBean(
+                    "default-repository");
             ((DefaultRepository) repository).setEntityType(type);
         }
 
